afeb1da523f23922015455359987f4506e94776f
ovidas00 <designerovi00@gmail.com>
2026-09-05 16:10:37 +0600

    updatd

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 import Cookies from "js-cookie";
+import { useUserStore } from "../store/useUserStore";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL as string;
 
@@ -16,5 +17,17 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove("accessToken");
+      useUserStore.getState().clearUser();
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
